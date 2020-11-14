44c99c20cbafab4f958d4d9521002e13b9790d93
Jono <jono.nicholas@hotmail.co.uk>
2020-11-14 15:26:21 +0000

    Added Stubbie class with login, set_url and add_header methods

--- stub.py
+++ stub.py
@@ -1,8 +1,51 @@
 #!/usr/bin/env python3
 import requests
 import base64
-import json
-from pprint import pprint
+
+
+class Stubbie():
+    def __init__(self, basic_token=None, username=None, password=None):
+        self.base_uri = 'https://api.stubhub.com/sellers'
+        self.api = self.login(basic_token, username, password)
+        self.token = self.api['access_token']
+        self.headers = {'Authorization': 'Bearer ' + self.token}
+
+    def set_url(self, url):
+        if url[0] == '/':
+            return self.base_uri + url
+        else:
+            return self.base_uri + '/' + url
+
+    def add_header(self, header, value):
+        self.headers[header] = value
+
+    def login(self, basic_token, username, password, retries=3):
+        creds = {
+            'basic_token': basic_token,
+            'username': username,
+            'password': password
+        }
+
+        if not all(creds.values()):
+            standard_creds = get_creds()
+        for cred, cred_value in creds.items():
+            if not cred_value:
+                creds[cred] = standard_creds[cred]
+
+        url = self.set_url('/oauth/accesstoken?grant_type=client_credentials')
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + creds['basic_token'],
+        }
+        data = {
+            'username': creds['username'],
+            'password': creds['password']
+        }
+        r = requests.post(url, headers=headers, json=data)
+        r.raise_for_status()
+
+        return r.json()
 
 
 def get_creds():
@@ -14,8 +57,11 @@ def get_creds():
     }
 
     for key, val in data.items():
-        with open(val) as f:
-            data[key] = f.read().strip()
+        try:
+            with open(val) as f:
+                data[key] = f.read().strip()
+        except OSError:
+            data[key] = None
 
     token_combo = data['consumer_key'] + ':' + data['consumer_secret']
     data['basic_token'] = str(base64.b64encode(token_combo.encode('utf-8')),
@@ -24,24 +70,6 @@ def get_creds():
     return data
 
 
-def login():
-    creds = get_creds()
-    url = 'https://api.stubhub.com/sellers/oauth/accesstoken?grant_type='
-    url += 'client_credentials'
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + creds['basic_token'],
-    }
-    data = {
-        'username': creds['username'],
-        'password': creds['password']
-    }
-    r = requests.post(url, headers=headers, json=data)
-
-    return r
-
-
 if __name__ == '__main__':
-    api = login()
-    print(api.status_code, api.reason, api.text)
+    stub = Stubbie()
+    print(stub.api)
